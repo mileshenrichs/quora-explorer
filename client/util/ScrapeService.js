89c4426185d54ddcf5429df9ade9ee930520af45
Miles <mileshenrichs21@gmail.com>
2018-12-17 15:44:58 -0600

    encode URL identifier before sending backend request

--- client/util/ScrapeService.js
+++ client/util/ScrapeService.js
@@ -2,7 +2,7 @@ class ScrapeService {
 
     static scrapeQuestion(qUrl) {
         const questionIdentifier = qUrl.substring(22);
-        return fetch('http://localhost:8080/api/explore?q=' + questionIdentifier);
+        return fetch('http://localhost:8080/api/explore?q=' + encodeURIComponent(questionIdentifier));
     }
 
 }

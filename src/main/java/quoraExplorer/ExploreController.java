ad7fb3a783a6d82f11a1a9ef4e00f9e3e4a0294c
Miles <mileshenrichs21@gmail.com>
2018-12-18 20:10:47 -0600

    begin integrating Selenium WebDriver, execute page scroll js

--- src/main/java/quoraExplorer/ExploreController.java
+++ src/main/java/quoraExplorer/ExploreController.java
@@ -1,5 +1,8 @@
 package quoraExplorer;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,14 @@ public class ExploreController {
             @RequestParam(value="q") String q
     ) {
         System.out.println(q);
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://quora.com/" + q);
+
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
         return new ScrapeInfo(-1, 100);
     }
 

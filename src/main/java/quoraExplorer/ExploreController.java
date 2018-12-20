7f68a06aecb1c63bc412dd302c0921f947906f29
Miles <mileshenrichs21@gmail.com>
2018-12-19 19:33:08 -0600

    start playing with sleeping & scrolling

--- src/main/java/quoraExplorer/ExploreController.java
+++ src/main/java/quoraExplorer/ExploreController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @CrossOrigin
 public class ExploreController {
@@ -16,14 +18,34 @@ public class ExploreController {
     public ScrapeInfo exploreQuestion(
             @RequestParam(value="q") String q
     ) {
-        System.out.println(q);
+        try {
+            System.out.println(q);
 
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://quora.com/" + q);
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://quora.com/" + q);
 
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            int prevPageSourceLength = Integer.MIN_VALUE;
+            String pageSource = "";
+
+            while(pageSource.length() != prevPageSourceLength) {
+                prevPageSourceLength = pageSource.length();
+
+                jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+                TimeUnit.MILLISECONDS.sleep(300);
+
+                jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight * .85)");
+
+                TimeUnit.MILLISECONDS.sleep(300);
+
+                pageSource = driver.getPageSource();
+                System.out.println(pageSource.length());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return new ScrapeInfo(-1, 100);
     }

cc7e5e196ec97b7bfa4dc883676409f03574a942
Miles <mileshenrichs21@gmail.com>
2018-12-29 15:05:10 -0600

    log requesting IP address on backend

--- src/main/java/quoraExplorer/ExploreController.java
+++ src/main/java/quoraExplorer/ExploreController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -13,17 +14,23 @@ public class ExploreController {
 
     @GetMapping("/api/top-rated-answer")
     public String getTopRatedAnswer(
-            @RequestParam(value="q") String q
+            @RequestParam(value="q") String q,
+            HttpServletRequest req
     ) {
+        System.out.println("Question id: " + q + " // Requested by " + getRequestingIPAddress(req));
         AnswerExtractor answerExtractor = new AnswerExtractor("https://quora.com/" + q);
         List<Answer> answers = answerExtractor.getAnswers();
         if(answers.isEmpty()) {
             return "--";
         }
-        System.out.println(answers.size() + " answers collected");
 
         AnswerListAnalyzer answerListAnalyzer = new AnswerListAnalyzer(answers);
         return answerListAnalyzer.pickHighestRatedAnswer();
+    }
+
+    private String getRequestingIPAddress(HttpServletRequest request) {
+        String xForwardedForHeader = request.getHeader("X-FORWARDED-FOR");
+        return xForwardedForHeader.substring(0, xForwardedForHeader.indexOf(","));
     }
 
 }

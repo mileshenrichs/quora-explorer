9eb1ee04b49548a6009737650a6975834588db18
Miles <mileshenrichs21@gmail.com>
2019-01-02 17:13:23 -0600

    upgrade scroll strategy: listen for changes in document height

--- src/main/java/quoraExplorer/AnswerExtractor.java
+++ src/main/java/quoraExplorer/AnswerExtractor.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Uses Selenium WebDriver and repeated scrolling to produce a list of all answers
@@ -34,28 +33,35 @@ public class AnswerExtractor {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         driver.get(questionUrl);
 
-        try {
-            PageParser parser = new PageParser(driver.getPageSource());
+        // To get answers to load on infinite scroll page, scroll down.
+        // Then, constantly listen for changes in the page's scroll height.
+        // When scroll height changes, it means that more answers have loaded.  It also means that we're not currently at bottom of page.
+        // So scroll down to bottom again and continue listening for more answers to be loaded.
+        // If no answers have loaded since 1.75 seconds after the last scroll, conclude that we've seen all answers and return
+        // Top answer is typically one of first ~36, so also stop after accumulating 36 answers (otherwise takes way too long for big questions)
+        final long MAX_TIME_TO_WAIT_FOR_LOAD_MS = 1750;
+        long previousScrollTimestamp = System.currentTimeMillis();
+        long previousScrollHeight = 0;
+        PageParser parser;
 
-            while(parser.getAnswerCount() > answers.size()) {
-                answers = parser.getAnswers();
-
-                // stop after collecting 36 answers,
-                // about 70% of the time we'll have already seen the highest rated answer
-                if(answers.size() >= 36) {
-                    break;
-                }
-
-                jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-                TimeUnit.SECONDS.sleep((long) 1.75);
-                parser = new PageParser(driver.getPageSource());
+        while(System.currentTimeMillis() - previousScrollTimestamp < MAX_TIME_TO_WAIT_FOR_LOAD_MS) {
+            parser = new PageParser(driver.getPageSource());
+            answers = parser.getAnswers();
+            // stop after collecting 36 answers,
+            // about 70% of the time we'll have already seen the highest rated answer
+            if(answers.size() >= 36) {
+                break;
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
+
+            long currentScrollHeight = (long) jsExecutor.executeScript("return document.body.scrollHeight");
+            if(currentScrollHeight > previousScrollHeight) {
+                jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                previousScrollTimestamp = System.currentTimeMillis();
+                previousScrollHeight = currentScrollHeight;
+            }
         }
 
+        driver.quit();
         return answers;
     }
 }
